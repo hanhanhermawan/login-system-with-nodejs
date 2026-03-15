7ca6bf8a7359cc1930d8f062e3c62846786aec4d
Hanhan Hermawan <hermawanhanhan@gmail.com>
2026-03-15 15:48:41 +0700

    refactor: update MongoDB connection and restructure main entry point; add user authentication routes

--- config/db.js
+++ config/db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO.substring, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected !!!");
   } catch (err) {
     console.error(err.message);
